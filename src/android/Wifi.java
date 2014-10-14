8cefc56512fecdf9b269e85af26d6b25f94fbab7
Ariel Schiavoni <arielschiavoni@gmail.com>
2014-10-14 11:22:04 +0200

    Add connect API

--- src/android/Wifi.java
+++ src/android/Wifi.java
@@ -6,6 +6,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.Context;
+import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.net.wifi.ScanResult;
 
@@ -14,6 +15,8 @@ import java.util.List;
 public class Wifi extends CordovaPlugin {
 
     private static final String SCAN = "scan";
+    private static final String CONNECT = "connect";
+
     private WifiManager wifiManager;
 
     @Override
@@ -26,21 +29,21 @@ public class Wifi extends CordovaPlugin {
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
         if (action.equals(SCAN)) {
             return this.scan(callbackContext);
+        } else if (action.equals(CONNECT)) {
+            return this.connect(callbackContext, data);
         }
         callbackContext.error("Incorrect action parameter: " + action);
         return false;
     }
 
     /**
-     *  This method uses the callbackContext.success method to send a JSONArray
-     *  of the scanned networks.
+     *  Scan wifi networks.
      *  @param  callbackContext A Cordova callback context
      *  @return true
      */
     private boolean scan(CallbackContext callbackContext) throws JSONException {
         List<ScanResult> scanResults = wifiManager.getScanResults();
         JSONArray returnList = new JSONArray();
-
         for (ScanResult scan : scanResults) {
             JSONObject wifi = new JSONObject();
             wifi.put("ssid", scan.SSID);
@@ -50,8 +53,42 @@ public class Wifi extends CordovaPlugin {
             wifi.put("level", scan.level);
             returnList.put(wifi);
         }
-
         callbackContext.success(returnList);
         return true;
+    }
+
+    /**
+     *  Connect to a network.
+     *  @param  callbackContext A Cordova callback context
+     *  @param  data JSON Array, with [0] being SSID to connect
+     *  @return true if network connected, false if failed
+     */
+    private boolean connect(CallbackContext callbackContext, JSONArray data) throws JSONException {
+        String ssid = data.getString(0);
+        int networkIdToConnect = getNetworkId(ssid);
+        if (networkIdToConnect > 0) {
+            wifiManager.disableNetwork(networkIdToConnect);
+            wifiManager.enableNetwork(networkIdToConnect, true);
+            callbackContext.success("Network " + ssid + " connected!");
+            return true;
+        }
+        else {
+            callbackContext.error("Network " + ssid + " not found!");
+            return false;
+        }
+    }
+
+    /**
+     *  Get the network id from the list of configured networks using SSID.
+     */
+    private int getNetworkId(String ssid) {
+        List<WifiConfiguration> currentNetworks = wifiManager.getConfiguredNetworks();
+        int networkId = -1;
+        for (WifiConfiguration test : currentNetworks) {
+            if ( test.SSID.equals(ssid) ) {
+                networkId = test.networkId;
+            }
+        }
+        return networkId;
     }
 }
--- www/wifi.js
+++ www/wifi.js
@@ -6,4 +6,8 @@ Wifi.prototype.scan = function (successCallback, errorCallback) {
 	cordova.exec(successCallback, errorCallback, "Wifi", "scan", []);
 };
 
+Wifi.prototype.connect = function (ssid, successCallback, errorCallback) {
+	cordova.exec(successCallback, errorCallback, "Wifi", "connect", [ssid]);
+};
+
 module.exports = new Wifi();
